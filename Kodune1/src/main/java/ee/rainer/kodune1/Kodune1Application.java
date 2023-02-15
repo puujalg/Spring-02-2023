be0b32d5c36f4288c9ef4828b9a3e58a1a868ae8
puujalg <124621163+puujalg@users.noreply.github.com>
2023-02-15 17:22:11 +0200

    Kodune1 added

---
+++ Kodune1/src/main/java/ee/rainer/kodune1/Kodune1Application.java
@@ -0,0 +1,15 @@
+package ee.rainer.kodune1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kodune1Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Kodune1Application.class, args);
+        
+    }
+
+}
---
+++ Kodune1/src/test/java/ee/rainer/kodune1/Kodune1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.rainer.kodune1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kodune1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
