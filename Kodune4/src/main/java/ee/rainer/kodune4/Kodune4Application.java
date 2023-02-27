5d888fe9d47c5f34cd0e475a8c2dbb3250f734a6
puujalg <124621163+puujalg@users.noreply.github.com>
2023-02-27 22:01:39 +0200

    Kodune4

---
+++ Kodune4/src/main/java/ee/rainer/kodune4/Kodune4Application.java
@@ -0,0 +1,13 @@
+package ee.rainer.kodune4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kodune4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kodune4Application.class, args);
+    }
+
+}
---
+++ Kodune4/src/test/java/ee/rainer/kodune4/Kodune4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.rainer.kodune4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kodune4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
